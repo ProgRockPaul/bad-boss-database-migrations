e13e3093d5fcec985396dd08ba15f5f3934f896c
Paul Walsh <twentytwentyk@live.com>
2017-02-02 15:44:13 -0500

    final

---
+++ db/migrate/20170202200442_change_quantity.rb
@@ -0,0 +1,16 @@
+class ChangeQuantity < ActiveRecord::Migration[5.0]
+  def change
+    reversible do |dir|
+      dir.up do
+        change_table :parts do |t|
+          t.change :quantity, :decimal
+        end
+      end
+      dir.down do
+        change_table :parts do |t|
+        t.change :quantity, :integer
+        end
+      end
+    end
+  end
+end
---
+++ db/migrate/20170202204127_address_to_locations_again.rb
@@ -0,0 +1,5 @@
+class AddressToLocationsAgain < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :locations, :weather, :address
+  end
+end
